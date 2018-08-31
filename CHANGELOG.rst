0789640091ad64e5eef8bc6850bc986fc717855a
Tzu-ping Chung <uranusjr@gmail.com>
2018-08-31 08:39:00 +0800

    Release 0.2.2

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,12 @@
+0.2.2 (2018-08-31)
+==================
+
+Bug Fixes
+---------
+
+- Fix compatibility to Python < 3.6.
+
+
 0.2.1 (2018-08-31)
 ==================
 
@@ -14,7 +23,7 @@ Features
 --------
 
 - Implement `__delitem__` to delete entries from a data view.  `#3 <https://github.com/sarugaku/plette/issues/3>`_
-  
+
 - Add slicing support to `DataViewSequence`. It is not possible to get, set, or
   delete a slice from it. For `__getitem__`, the return value’s type would match
   the sliced `DataViewSequence`.  `#4 <https://github.com/sarugaku/plette/issues/4>`_
@@ -27,17 +36,17 @@ Features
 --------
 
 - Lock file dumps are now guarenteed to end with a trailing newline.  `#2 <https://github.com/sarugaku/plette/issues/2>`_
-  
+
 
 Bug Fixes
 ---------
 
 - Fix AttributeError in `Lockfile.is_up_to_date()`  `#1 <https://github.com/sarugaku/plette/issues/1>`_
-  
+
 - Fix `Lockfile.dump()` to work correctly on Python 2 when outputting to `io`.  `#2 <https://github.com/sarugaku/plette/issues/2>`_
 
 
 0.1.0 (2018-08-16)
 ==================
 
-No significant changes.
+Initial release.
--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,6 +1,6 @@
+import ast
 import pathlib
 import shutil
-import subprocess
 
 import invoke
 import parver
@@ -25,14 +25,12 @@ def clean(ctx):
 
 
 def _read_version():
-    out = subprocess.check_output(['git', 'tag'], encoding='ascii')
-    try:
-        version = max(parver.Version.parse(v).normalize() for v in (
-            line.strip() for line in out.split('\n')
-        ) if v)
-    except ValueError:
-        version = parver.Version.parse('0.0.0')
-    return version
+    with INIT_PY.open() as f:
+        for line in f:
+            if not line.startswith('__version__ = '):
+                continue
+            value = ast.literal_eval(line.split('=', 1)[-1].strip())
+            return parver.Version.parse(value)
 
 
 def _write_version(v):
@@ -49,44 +47,24 @@ def _write_version(v):
 REL_TYPES = ('major', 'minor', 'patch',)
 
 
-def _bump_release(version, type_):
-    next_version = version.base_version().bump_release(type_)
-    print(f'[bump] {version} -> {next_version}')
-    return next_version
-
-
-def _prebump(version, prebump):
-    next_version = version.bump_release(prebump).bump_dev()
-    print(f'[bump] {version} -> {next_version}')
-    return next_version
-
-
-PREBUMP = 'patch'
+@invoke.task()
+def bump_release(ctx, type_):
+    if type_ not in REL_TYPES:
+        raise ValueError(f'{type_} not in {REL_TYPES}')
+    index = REL_TYPES.index(type_)
+    prev_version = _read_version()
+    next_version = prev_version.base_version().bump_release(index)
+    print(f'[bump] {prev_version} -> {next_version}')
+    _write_version(next_version)
 
 
 @invoke.task(pre=[clean])
-def release(ctx, type_, repo, prebump=PREBUMP):
-    """Make a new release.
-    """
-    if type_ not in REL_TYPES:
-        raise ValueError(f'{type_} not in {REL_TYPES}')
-    type_ = REL_TYPES.index(type_)
-    if prebump not in REL_TYPES:
-        raise ValueError(f'{type_} not in {REL_TYPES}')
-    prebump = REL_TYPES.index(prebump)
-
-    version = _read_version()
-    version = _bump_release(version, type_)
-    _write_version(version)
-
-    ctx.run('towncrier')
-
-    ctx.run(f'git commit -am "Release {version}"')
-
-    ctx.run(f'git tag -a {version} -m "Version {version}"')
-
+def build(ctx):
     ctx.run(f'python setup.py sdist bdist_wheel')
 
+
+@invoke.task(pre=[build])
+def upload(ctx, repo):
     dist_pattern = f'{PACKAGE_NAME.replace("-", "[-_]")}-*'
     artifacts = list(ROOT.joinpath('dist').glob(dist_pattern))
     filename_display = '\n'.join(f'  {a}' for a in artifacts)
@@ -100,23 +78,38 @@ def release(ctx, type_, repo, prebump=PREBUMP):
     arg_display = ' '.join(f'"{n}"' for n in artifacts)
     ctx.run(f'twine upload --repository="{repo}" {arg_display}')
 
-    version = _prebump(version, prebump)
-    _write_version(version)
 
-    ctx.run(f'git commit -am "Prebump to {version}"')
+@invoke.task()
+def prebump(ctx, type_):
+    if type_ not in REL_TYPES:
+        raise ValueError(f'{type_} not in {REL_TYPES}')
+    index = REL_TYPES.index(type_)
+    prev_version = _read_version()
+    next_version = prev_version.bump_release(index).bump_dev()
+    print(f'[bump] {prev_version} -> {next_version}')
+    _write_version(next_version)
 
 
-@invoke.task
-def build_docs(ctx):
-    _current_version = _read_version()
-    minor = [str(i) for i in _current_version[:2]]
-    docs_folder = (ROOT / 'docs').as_posix()
-    if not docs_folder.endswith('/'):
-        docs_folder = '{0}/'.format(docs_folder)
-    args = ["--ext-autodoc", "--ext-viewcode", "-o", docs_folder]
-    args.extend(["-A", "'Tzu-ping Chung <uranusjr@gmail.com>'"])
-    args.extend(["-R", _current_version])
-    args.extend(["-V", ".".join(minor)])
-    args.extend(["-e", "-M", "-F", f"src/{PACKAGE_NAME}"])
-    print("Building docs...")
-    ctx.run("sphinx-apidoc {0}".format(" ".join(args)))
+PREBUMP = 'patch'
+
+
+@invoke.task()
+def release(ctx, type_, repo=None, prebump_to=PREBUMP):
+    """Make a new release.
+    """
+    bump_release(ctx, type_=type_)
+
+    this_version = _read_version()
+    ctx.run('towncrier')
+    ctx.run(f'git commit -am "Release {this_version}"')
+    ctx.run(f'git tag -fa {this_version} -m "Version {this_version}"')
+
+    if repo:
+        upload(ctx, repo=repo)
+    else:
+        print('[release] Missing --repo, skip uploading')
+
+    prebump(ctx, type_=prebump_to)
+
+    next_version = _read_version()
+    ctx.run(f'git commit -am "Prebump to {next_version}"')
