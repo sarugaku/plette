73d923fba3461627d8fcd21f1850aee50b91f7f1
Oz Tiram <oz.tiram@gmail.com>
2024-04-19 10:34:07 +0200

    Migrate more classes to DataModel
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -5,12 +5,13 @@ import pytest
 import plette
 from plette import Pipfile
 
+from plette.models.base import ValidationError
 invalid_files = glob.glob("examples/*invalid*")
 valid_files = glob.glob("examples/*ok*")
 
 @pytest.mark.parametrize("fname", invalid_files)
 def test_invalid_files(fname):
-    with pytest.raises(plette.models.base.ValidationError) as excinfo:
+    with pytest.raises((ValueError, plette.models.base.ValidationError, ValidationError)) as excinfo:
         with open(fname) as f:
             pipfile = Pipfile.load(f)
 
